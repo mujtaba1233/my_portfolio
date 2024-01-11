145e6693c0a78df2d49fcac12162cbe385a64f97
mujtaba1233 <shahzadmujtaba316@gmail.com>
2024-01-12 00:08:15 +0500

    asjnkjas

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,5 +4,5 @@ export const headerData = {
     title: "Hello! I am",
     desciption: "Hello! I am MUJTABA SHAHZAD, as a full-stack developer with expertise in MERN and MEAN stacks, I thrive on building dynamic web applications that empower businesses to streamline operations, optimize workflows, and engage their audiences.",
     image: 'yellow.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/15te91Gu4Qtfuz1Oab3F1gsaN2ANTDw3Z/view?usp=sharing'
 }
